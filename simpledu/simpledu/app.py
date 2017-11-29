f43e8c5928e6427e5bf6e5e22ed6b03bfe1fb8ad
Manchangdx <1195581533@qq.com>
2017-11-30 01:13:27 +0800

    fix #12 修改数据库表，使用 flask-migrate 管理数据库版本

--- simpledu/simpledu/app.py
+++ simpledu/simpledu/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate
 from .models import db, Course, User
 from .config import configs
 
@@ -13,5 +14,6 @@ def create_app(config):
     app.config.from_object(configs.get(config))
     db.init_app(app)
     register_blueprints(app)
+    Migrate(app, db)
 
     return app
