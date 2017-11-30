301c792d2ab40fa3e24220c9fe1e62c4e1f3b318
Chuntianlaile <2039423065@qq.com>
2017-11-30 19:31:36 +0800

    fix #8 弄了个简单框架

---
+++ simpledu/simpledu/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from .models import db, Course, User
+from .config import configs
+
+def register_blueprints(a):
+    from .handlers import admin, course, front
+    a.register_blueprint(admin)
+    a.register_blueprint(course)
+    a.register_blueprint(front)
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(configs.get(config))
+    db.init_app(app)
+    register_blueprints(app)
+
+    return app
